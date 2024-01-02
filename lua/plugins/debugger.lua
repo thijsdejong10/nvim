b85624f67ea3bd0422c8e62e0937209e6c7721cd
thijsdejong10 <thijsdejong10@gmail.com>
2024-01-02 10:24:46 +0100

    added cmp-dap

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -1,4 +1,21 @@
 return {
+  {
+    "rcarriga/cmp-dap",
+  },
+  {
+    require("cmp").setup({
+      enabled = function()
+        return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
+      end,
+    }),
+  },
+  {
+    require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
+      sources = {
+        { name = "dap" },
+      },
+    }),
+  },
   -- "mfussenegger/nvim-dap-python",
   -- config = function()
   --   require("dap-python").setup("~/miniconda3/envs/rc/bin/python")

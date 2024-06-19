10cc0b3b68a26557f16eea000691cfeff63097d0
Thijs <thijsdejong10@gmail.com>
2024-06-19 20:37:32 +0200

    added bindings for textobjects

---
+++ lua/plugins/nvim-treesitter-textobjects.lua
@@ -0,0 +1,23 @@
+return {
+  "nvim-treesitter/nvim-treesitter-textobjects",
+  lazy = true,
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      textobjects = {
+        swap = {
+          enable = true,
+          swap_next = {
+            ["<leader>na"] = "@parameter.inner", -- swap parameters/argument with next
+            ["<leader>n:"] = "@property.outer", -- swap object property with next
+            ["<leader>nm"] = "@function.outer", -- swap function with next
+          },
+          swap_previous = {
+            ["<leader>pa"] = "@parameter.inner", -- swap parameters/argument with prev
+            ["<leader>p:"] = "@property.outer", -- swap object property with prev
+            ["<leader>pm"] = "@function.outer", -- swap function with previous
+          },
+        },
+      },
+    })
+  end,
+}

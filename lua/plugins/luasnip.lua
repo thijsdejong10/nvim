27e860ee82959999a0a3ec01e2d60d19ce2e783e
Thijs <thijsdejong10@gmail.com>
2024-09-15 17:26:47 +0200

    Removed luasnip config as luasnip is no longer in the lazyvim packages

--- lua/plugins/luasnip.lua
+++
@@ -1,5 +0,0 @@
-local luasnip = require("luasnip")
-return {
-  luasnip.add_snippets("tex", { luasnip.snippet("cacl2", { luasnip.text_node("CaCl$_2$") }) }),
-  luasnip.add_snippets("tex", { luasnip.snippet("caoh2", { luasnip.text_node("Ca(OH)$_2$") }) }),
-}
